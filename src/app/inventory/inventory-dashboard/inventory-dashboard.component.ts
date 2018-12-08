0afcf1d00a652d4a2ddf84eaa085925d5cd1798c
owen_adira <owenobezzy@gmail.com>
2018-12-08 20:10:27 +0300

    invetory and pos  module components

---
+++ src/app/inventory/inventory-dashboard/inventory-dashboard.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory-dashboard',
+  template: `
+    <p>
+      inventory-dashboard works!
+    </p>
+  `,
+  styles: []
+})
+export class InventoryDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/inventory/stock-entry/stock-entry.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-entry',
+  template: `
+    <p>
+      stock-entry works!
+    </p>
+  `,
+  styles: []
+})
+export class StockEntryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
