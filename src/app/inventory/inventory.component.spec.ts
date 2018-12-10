a4cfbac5f93baacfb8618cc5e153a9ad19ecf786
owen_adira <owenobezzy@gmail.com>
2018-12-10 20:02:10 +0300

    update inventory module to use common config tests file

--- src/app/inventory/inventory.component.spec.ts
+++ src/app/inventory/inventory.component.spec.ts
@@ -1,25 +1,27 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { InventoryComponent } from './inventory.component';
+import { InventoryComponent } from './inventory.component'
+import { commonTestingModules } from '../common/common.testing'
 
 describe('InventoryComponent', () => {
-  let component: InventoryComponent;
-  let fixture: ComponentFixture<InventoryComponent>;
+  let component: InventoryComponent
+  let fixture: ComponentFixture<InventoryComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: commonTestingModules,
       declarations: [ InventoryComponent ]
     })
-    .compileComponents();
-  }));
+    .compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(InventoryComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(InventoryComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
