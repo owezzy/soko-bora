e64bf43d7411b6f64dce470ca472ad3b365f223f
owen_adira <owenobezzy@gmail.com>
2018-12-11 12:46:35 +0300

    update e2e tests config for continous intergration

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,7 +8,7 @@ describe('workspace-project App', () => {
   });
 
   it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to soko-bora!');
-  });
+    page.navigateTo()
+    expect(page.getParagraphText()).toEqual('Soko Bora')
+  })
 });
