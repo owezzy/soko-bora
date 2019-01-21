66002d24aeeaa5232539ad7d57edc5fea2ae5f87
owezzy <owenobezzy@gmail.com>
2019-01-21 12:39:50 +0300

    lint

--- src/app/common/simple-dialog/simple-dialog.component.ts
+++ src/app/common/simple-dialog/simple-dialog.component.ts
@@ -7,13 +7,14 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material'
     <mat-dialog-content>
       <p>data.content</p>
     </mat-dialog-content>
-      <mat-dialog-actions>
-        <span class="flex-spacer"></span>
-        <button mat-button mat-dialog-close
-        *ngIf="data.cancelText">data.cancelText</button>
-        <button mat-button mat-button-raised color="primary"
-                [mat-dialog-close]="true" cdkFocusInitial></button>
-      </mat-dialog-actions>
+    <mat-dialog-actions>
+      <span class="flex-spacer"></span>
+      <button mat-button mat-dialog-close
+              *ngIf="data.cancelText">data.cancelText
+      </button>
+      <button mat-button mat-raised-button color="primary"
+              [mat-dialog-close]="true"></button>
+    </mat-dialog-actions>
   `,
   styles: []
 })
