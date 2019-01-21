9d0e7f454c10e43e5952878b2cab5af802540f57
owezzy <owenobezzy@gmail.com>
2019-01-21 10:51:30 +0300

    autohide icons for unauthorized user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { MatIconRegistry } from '@angular/material'
 import { DomSanitizer } from '@angular/platform-browser'
+import { AuthService } from './auth/auth.service'
 
 @Component({
   selector: 'app-root',
   template: `
     <mat-toolbar color="primary" fxLayoutGap="18px">
-      <button mat-icon-button><mat-icon>menu</mat-icon></button>
+      <button *ngIf="displayAccountIcons" mat-icon-button><mat-icon>menu</mat-icon></button>
       <a mat-button routerLink="/home"><mat-icon svgIcon="grocery"></mat-icon><span class="mat-h2"> Soko Bora</span></a>
       <span class="flex-spacer"></span>
-      <button
+      <button *ngIf="displayAccountIcons"
         mat-mini-fab
         routerLink="/user/profile"
         matTooltip="Profile"
@@ -17,7 +18,7 @@ import { DomSanitizer } from '@angular/platform-browser'
       >
         <mat-icon>account_circle</mat-icon>
       </button>
-      <button
+      <button *ngIf="displayAccountIcons"
         mat-mini-fab
         routerLink="/user/logout"
         matTooltip="Logout"
@@ -29,13 +30,20 @@ import { DomSanitizer } from '@angular/platform-browser'
     <router-outlet></router-outlet>
   `,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'soko-bora'
+  displayAccountIcons = false
 
-  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+  constructor( private authService: AuthService, iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
     iconRegistry.addSvgIcon(
       'grocery',
       sanitizer.bypassSecurityTrustResourceUrl('assets/img/icons/grocery.svg')
+    )
+  }
+
+  ngOnInit() {
+    this.authService.authStatus.subscribe(
+      authStatus => (this.displayAccountIcons = authStatus.isAuthenticated)
     )
   }
 }
