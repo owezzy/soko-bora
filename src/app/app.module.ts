a1251ac6b07fa002d1988a79a8ec476290acf85e
owezzy <owenobezzy@gmail.com>
2019-01-21 12:14:00 +0300

    add simple dialog to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,11 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { FlexLayoutModule } from '@angular/flex-layout'
 import { AuthService } from './auth/auth.service'
 import { LoginComponent } from './login/login.component'
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SimpleDialogComponent } from './common/simple-dialog/simple-dialog.component'
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, PageNotFoundComponent, LoginComponent],
+  declarations: [AppComponent, HomeComponent, PageNotFoundComponent, LoginComponent, SimpleDialogComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
