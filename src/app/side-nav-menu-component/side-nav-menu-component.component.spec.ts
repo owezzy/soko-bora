846736231582a969b435a84ed70c7e0eb39a2de1
owezzy <owenobezzy@gmail.com>
2019-01-23 22:03:12 +0300

    generate side-nav component

---
+++ src/app/side-nav-menu-component/side-nav-menu-component.component.spec.ts
@@ -0,0 +1,42 @@
+import { LayoutModule } from '@angular/cdk/layout';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatListModule,
+  MatSidenavModule,
+  MatToolbarModule,
+} from '@angular/material';
+
+import { SideNavMenuComponentComponent } from './side-nav-menu-component.component';
+
+describe('SideNavMenuComponentComponent', () => {
+  let component: SideNavMenuComponentComponent;
+  let fixture: ComponentFixture<SideNavMenuComponentComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [SideNavMenuComponentComponent],
+      imports: [
+        NoopAnimationsModule,
+        LayoutModule,
+        MatButtonModule,
+        MatIconModule,
+        MatListModule,
+        MatSidenavModule,
+        MatToolbarModule,
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SideNavMenuComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/side-nav-menu-component/side-nav-menu-component.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-side-nav-menu-component',
+  templateUrl: './side-nav-menu-component.component.html',
+  styleUrls: ['./side-nav-menu-component.component.scss'],
+})
+export class SideNavMenuComponentComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches)
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
