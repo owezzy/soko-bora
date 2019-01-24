f7df4d7858e6171d69293b60bd9b3d63b595322b
owezzy <owenobezzy@gmail.com>
2019-01-24 17:56:22 +0300

    define auth guard using role base defination

---
+++ src/app/auth/auth-guard.guard.ts
@@ -0,0 +1,76 @@
+import { Injectable } from '@angular/core'
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  CanLoad, CanActivateChild, Router,
+} from '@angular/router'
+import { Observable } from 'rxjs'
+import { AuthService, IAuthStatus } from './auth.service'
+import { Route } from '@angular/compiler/src/core'
+import { Role } from './role.enum'
+import { UiService } from '../common/ui.service'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
+  protected currentAuthStatus: IAuthStatus
+
+  constructor(
+    protected authService: AuthService,
+    protected router: Router,
+    private uiService: UiService,
+  ) {
+    this.authService.authStatus.subscribe(
+      authStatus => (this.currentAuthStatus = authStatus),
+    )
+  }
+
+  canLoad(route: Route): boolean | Observable<boolean> | Promise<boolean> {
+    return this.checkLogin()
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
+    return this.checkLogin(route)
+  }
+
+  canActivateChild(
+    childRoute: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
+    return this.checkLogin(childRoute)
+  }
+
+  protected checkLogin(route?: ActivatedRouteSnapshot) {
+    let roleMatch = true
+    let params: any
+    if (route) {
+      const expectedRole = route.data.expectedRole
+      if (expectedRole) {
+        roleMatch = this.currentAuthStatus.userRole === expectedRole
+      }
+      if (expectedRole) {
+        params = { redirectUrl: route.pathFromRoot.map(r => r.url).join('/') }
+      }
+    }
+    if (!this.currentAuthStatus.isAuthenticated || !roleMatch) {
+      this.showAlert(this.currentAuthStatus.isAuthenticated, roleMatch)
+
+      this.router.navigate(['login', params || {}])
+      return false
+    }
+    return true
+  }
+
+  private showAlert(isAuth: boolean, roleMatch: boolean) {
+    if (!isAuth) {
+      this.uiService.showToast('You must login to continue')
+    }
+
+    if (!roleMatch) {
+      this.uiService.showToast('Permission Denied!')
+    }
+  }
+}
