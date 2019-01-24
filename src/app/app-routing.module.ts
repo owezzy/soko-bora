86d4895a46145954e82b002ab330f50abcb892ed
owezzy <owenobezzy@gmail.com>
2019-01-24 18:02:25 +0300

    use auth guard to restrict lazy loading to roles routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { RouterModule, Routes } from '@angular/router'
 import { HomeComponent } from './home/home.component'
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 import { LoginComponent } from './login/login.component'
+import { AuthGuard } from './auth/auth-guard.guard'
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'manager', loadChildren: './manager/manager.module#ManagerModule' },
+  {
+    path: 'manager',
+    loadChildren: './manager/manager.module#ManagerModule',
+    canLoad: [AuthGuard],
+  },
   { path: 'user', loadChildren: './user/user.module#UserModule' },
   { path: 'pos', loadChildren: './pos/pos.module#PosModule' },
   { path: 'inventory', loadChildren: './inventory/inventory.module#InventoryModule' },
@@ -20,4 +25,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
