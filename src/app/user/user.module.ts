5d5e69ff739171ad82a6a9430b2482795f8ac029
owezzy <owenobezzy@gmail.com>
2019-01-29 14:26:55 +0300

    update tests to use fake service

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,10 +3,9 @@ import { NgModule } from '@angular/core'
 import { UserRoutingModule } from './user-routing.module';
 import { ProfileComponent } from './profile/profile.component';
 import { LogoutComponent } from './logout/logout.component';
-import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component'
 
 @NgModule({
-  declarations: [ProfileComponent, LogoutComponent, NavigationMenuComponent],
+  declarations: [ProfileComponent, LogoutComponent],
   imports: [CommonModule, UserRoutingModule],
 })
 export class UserModule {}
