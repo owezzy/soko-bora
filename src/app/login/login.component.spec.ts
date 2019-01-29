e224bdd0a028e078cd2525bed3f448db203e2443
owezzy <owenobezzy@gmail.com>
2019-01-29 14:31:13 +0300

    use authguard test providers

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { LoginComponent } from './login.component'
+import { commonTestingModules, commonTestingProviders } from '../common/common.testing'
+import { AuthServiceFake } from '../auth/auth.service.fake'
+import { AuthGuard } from '../auth/auth-guard.guard'
 
 describe('LoginComponent', () => {
   let component: LoginComponent
@@ -8,6 +11,8 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [commonTestingModules],
+      providers: [ commonTestingProviders ],
       declarations: [ LoginComponent ]
     })
     .compileComponents()
