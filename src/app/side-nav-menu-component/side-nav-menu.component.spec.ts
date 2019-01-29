a091c85a0ab62bd112e688373072174186fd1809
owezzy <owenobezzy@gmail.com>
2019-01-29 14:27:12 +0300

    update tests to use fake service

--- src/app/side-nav-menu-component/side-nav-menu.component.spec.ts
+++ src/app/side-nav-menu-component/side-nav-menu.component.spec.ts
@@ -1,42 +1,29 @@
-import { LayoutModule } from '@angular/cdk/layout';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import {
-  MatButtonModule,
-  MatIconModule,
-  MatListModule,
-  MatSidenavModule,
-  MatToolbarModule,
-} from '@angular/material';
-
-import { SideNavMenuComponent } from './side-nav-menu.component';
+import { LayoutModule } from '@angular/cdk/layout'
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { SideNavMenuComponent } from './side-nav-menu.component'
+import { AppComponent } from '../app.component'
+import { commonTestingModules, commonTestingProviders } from '../common/common.testing'
+import { AuthGuard } from '../auth/auth-guard.guard'
 
 describe('SideNavMenuComponent', () => {
-  let component: SideNavMenuComponent;
-  let fixture: ComponentFixture<SideNavMenuComponent>;
+  let component: SideNavMenuComponent
+  let fixture: ComponentFixture<SideNavMenuComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [SideNavMenuComponent],
-      imports: [
-        NoopAnimationsModule,
-        LayoutModule,
-        MatButtonModule,
-        MatIconModule,
-        MatListModule,
-        MatSidenavModule,
-        MatToolbarModule,
-      ]
-    }).compileComponents();
-  }));
+      declarations: [SideNavMenuComponent, AppComponent],
+      imports: [commonTestingModules],
+      providers: commonTestingProviders,
+    }).compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SideNavMenuComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(SideNavMenuComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should compile', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
