8ce56d887bd03ecb17050c50dec57b37704cc8ac
owezzy <owenobezzy@gmail.com>
2019-01-30 15:47:30 +0300

    add user-material to main user module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,13 +1,25 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
-import { UserRoutingModule } from './user-routing.module';
-import { ProfileComponent } from './profile/profile.component';
-import { LogoutComponent } from './logout/logout.component';
+import { UserRoutingModule } from './user-routing.module'
+import { ProfileComponent } from './profile/profile.component'
+import { LogoutComponent } from './logout/logout.component'
 import { User } from './user'
+import { UserMaterialModule } from './user-material.module'
+import { AppMaterialModule } from '../app.material.module'
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [ProfileComponent, LogoutComponent],
-  imports: [CommonModule, UserRoutingModule],
-  providers: [User]
+  imports: [
+    CommonModule,
+    UserRoutingModule,
+    UserMaterialModule,
+    AppMaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule],
+  providers: [User],
 })
-export class UserModule {}
+export class UserModule {
+}
