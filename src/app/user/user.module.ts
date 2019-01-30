5d10e375fd41814db7fbdf1426d4e5fb44b4c330
owezzy <owenobezzy@gmail.com>
2019-01-30 07:43:05 +0300

    Add user class to user module providers

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core'
 import { UserRoutingModule } from './user-routing.module';
 import { ProfileComponent } from './profile/profile.component';
 import { LogoutComponent } from './logout/logout.component';
+import { User } from './user'
 
 @NgModule({
   declarations: [ProfileComponent, LogoutComponent],
   imports: [CommonModule, UserRoutingModule],
+  providers: [User]
 })
 export class UserModule {}
