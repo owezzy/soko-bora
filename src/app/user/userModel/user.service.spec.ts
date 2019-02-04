c4fff195d836d0b11f38c381ab1ba6bb19ceb13e
owezzy <owenobezzy@gmail.com>
2019-02-04 14:23:04 +0300

    add user input account info  step form markup

--- src/app/user/userModel/user.service.spec.ts
+++ src/app/user/userModel/user.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing'
 
-import { UserService } from './user.service'
+import { UserService } from './userModel/user.service'
 
 describe('UserService', () => {
   beforeEach(() => TestBed.configureTestingModule({}))
