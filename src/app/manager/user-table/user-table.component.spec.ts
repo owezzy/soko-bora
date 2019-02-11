053359756f256f3670c80f7b5ca9fb5416330115
owezzy <owenobezzy@gmail.com>
2019-02-11 18:03:41 +0300

    import and provide common testing modules and datasource for datatable

--- src/app/manager/user-table/user-table.component.spec.ts
+++ src/app/manager/user-table/user-table.component.spec.ts
@@ -1,25 +1,33 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { UserTableComponent } from './user-table.component';
+import { UserTableComponent } from './user-table.component'
+import { MatTableDataSource } from '@angular/material'
+import { User } from '../../user/userModel/user'
+import { commonTestingModules, commonTestingProviders } from '../../common/common.testing'
 
 describe('UserTableComponent', () => {
-  let component: UserTableComponent;
-  let fixture: ComponentFixture<UserTableComponent>;
+  let component: UserTableComponent
+  let fixture: ComponentFixture<UserTableComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserTableComponent ]
+      declarations: [ UserTableComponent ],
+      providers: commonTestingProviders,
+      imports: commonTestingModules,
     })
-    .compileComponents();
-  }));
+    .compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(UserTableComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(UserTableComponent)
+    component = fixture.componentInstance
+    component.dataSource = new MatTableDataSource()
+    component.dataSource.data = [new User()]
+    component._skipLoading = true
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
