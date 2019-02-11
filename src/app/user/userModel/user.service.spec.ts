9021d273fe46c5497db6a29630bfd62d25a913f2
owezzy <owenobezzy@gmail.com>
2019-02-11 18:08:28 +0300

    import modules for testing

--- src/app/user/userModel/user.service.spec.ts
+++ src/app/user/userModel/user.service.spec.ts
@@ -1,9 +1,18 @@
 import { TestBed } from '@angular/core/testing'
 
 import { UserService } from './user.service'
+import { AuthService } from '../../auth/auth.service'
+import { AuthServiceFake } from '../../auth/auth.service.fake'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 
 describe('UserService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}))
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      UserService,
+      { provide: AuthService, useClass: AuthServiceFake }
+      ],
+    imports: [HttpClientTestingModule]
+  }))
 
   it('should be created', () => {
     const service: UserService = TestBed.get(UserService)
