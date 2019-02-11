02ee8c6a8284b42ef39a2ef263671538a5289a35
owezzy <owenobezzy@gmail.com>
2019-02-11 17:57:55 +0300

    add user material module  in testing module

--- src/app/app.material.module.ts
+++ src/app/app.material.module.ts
@@ -15,6 +15,7 @@ import {
   MatSortModule,
   MatTableModule,
   MatToolbarModule,
+  MatDatepickerModule,
 } from '@angular/material'
 
 @NgModule({
@@ -35,6 +36,7 @@ import {
     MatSortModule,
     MatPaginatorModule,
     MatProgressSpinnerModule
+
   ],
   exports: [
     CommonModule,
@@ -51,7 +53,8 @@ import {
     MatTableModule,
     MatSortModule,
     MatPaginatorModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatDatepickerModule,
   ],
 })
 export class AppMaterialModule {}
