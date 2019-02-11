3f31c5d80e3cc417a230af7c97127e8bb0190fc2
owezzy <owenobezzy@gmail.com>
2019-02-11 18:09:25 +0300

    import modules for testing

--- src/app/user/view-user/view-user.component.spec.ts
+++ src/app/user/view-user/view-user.component.spec.ts
@@ -1,25 +1,37 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { ViewUserComponent } from './view-user.component';
+import { ViewUserComponent } from './view-user.component'
+import { ReactiveFormsModule } from '@angular/forms'
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { AppMaterialModule } from '../../app.material.module'
+import { UserMaterialModule } from '../user-material.module'
+import { RouterTestingModule } from '@angular/router/testing'
 
 describe('ViewUserComponent', () => {
-  let component: ViewUserComponent;
-  let fixture: ComponentFixture<ViewUserComponent>;
+  let component: ViewUserComponent
+  let fixture: ComponentFixture<ViewUserComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ViewUserComponent ]
+      declarations: [ ViewUserComponent ],
+     imports: [
+       ReactiveFormsModule,
+       FlexLayoutModule,
+       AppMaterialModule,
+       UserMaterialModule,
+       RouterTestingModule
+     ]
     })
-    .compileComponents();
-  }));
+    .compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ViewUserComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(ViewUserComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
